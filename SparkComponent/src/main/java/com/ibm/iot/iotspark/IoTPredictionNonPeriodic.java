cb3fe9b5eaf10e8795adc3e5a37aac61dbb77e63
Hokiman Kurniawan <hokiman.kurniawan@gmail.com>
2017-07-24 14:35:47 +0700

    Change the variable name

--- SparkComponent/src/main/java/com/ibm/iot/iotspark/IoTPredictionNonPeriodic.java
+++ SparkComponent/src/main/java/com/ibm/iot/iotspark/IoTPredictionNonPeriodic.java
@@ -27,7 +27,7 @@ import org.apache.wink.json4j.JSONObject;
 
 @SuppressWarnings("serial")
 public class IoTPredictionNonPeriodic extends IoTPrediction {
-	
+
 	/**
 	 * A formatter for ISO 8601 compliant timestamps.
 	 */
@@ -36,31 +36,31 @@ public class IoTPredictionNonPeriodic extends IoTPrediction {
     private JSONObject data = null;
     private int cycle = 1;
     private int windowsize = 0;
-    
+
     private JSONArray pa = new JSONArray();
 	private int count = 0;
 	private String pdt;
 
 	private IoTZScore zscoreObj = null;
-	
+
     IoTPredictionNonPeriodic() {
     	System.out.println("Creating new instance of IoTPredictionNonPeriodic");
     	zscoreObj = new IoTZScore();
     }
-    
+
     /**
      * This is a test method used for verifying the configuration parameters.
-     * @param predictiveServiceURL 
-     * @throws Exception 
-     * @throws JSONException 
-     * @throws FileNotFoundException 
+     * @param predictiveServiceURL
+     * @throws Exception
+     * @throws JSONException
+     * @throws FileNotFoundException
      */
     public static boolean testScoring(String predictiveServiceURL) throws FileNotFoundException, JSONException, Exception {
     	IoTPredictionNonPeriodic p = new IoTPredictionNonPeriodic();
     	p.load(10, 10);
     	p.setAccessURL(predictiveServiceURL);
     	String ret = post(p.getAccessURL(), p.data.toString());
-    	
+
     	if(ret.contains("Failed to score")) {
     		System.out.println("Looks like an issue with the URL, Please check the AccessKey and context id!!");
     		return false;
@@ -72,7 +72,7 @@ public class IoTPredictionNonPeriodic extends IoTPrediction {
 
     /*
      * Load historical data that required by the modeler and build a JSON object.
-     * 
+     *
      */
     public void load(int number) throws FileNotFoundException, JSONException, Exception {
         try {
@@ -87,7 +87,7 @@ public class IoTPredictionNonPeriodic extends IoTPrediction {
 
     /*
      * Load historical data that required by the modeler and build a JSON object.
-     * 
+     *
      */
     public  void load(int number, int size) throws FileNotFoundException, JSONException, Exception {
         try {
@@ -101,9 +101,9 @@ public class IoTPredictionNonPeriodic extends IoTPrediction {
     		// Ignore - the exception might be thrown incase if there is no historic data
     	}
     }
-        
+
     public void appendDataSet(JSONObject obj) throws JSONException {
-        double d = obj.getDouble("temperature");
+        double d = obj.getDouble("vibration");
 
         try {
 	        JSONArray ja = (JSONArray) data.get("data");
@@ -115,13 +115,13 @@ public class IoTPredictionNonPeriodic extends IoTPrediction {
 	        ja.put(na);
         } catch(Exception e) {
         	// Ignore - the exception might be thrown incase if there is no historic data
-        }        
+        }
         return;
-    	
+
     }
 
     /*
-     * Make prediction based on a input string. The string has to be 
+     * Make prediction based on a input string. The string has to be
      * in JSON format
      */
     public String predict(String s) throws Exception {
@@ -129,33 +129,33 @@ public class IoTPredictionNonPeriodic extends IoTPrediction {
     	if(data == null) {
     		load(this.cycle, this.windowsize);
     	}
-    	
+
        Double forecast = 0.0;
        JSONObject pdt = new JSONObject();
-       
+
        if (s == null)
            return null;
-      
+
        // The Watson IoT Java client library sends the data like {d: {payload}}
        JSONObject obj = (JSONObject)JSON.parse(s);
        if(obj.has("d")) {
     	   obj = obj.getJSONObject("d");
        }
-       //put device temperature string into prediction dataset
-       if (obj.has("name")&&obj.get("name") != null && 
-           obj.has("temperature") && obj.get("temperature") != null )  {
+       //put device vibration string into prediction dataset
+       if (obj.has("name")&&obj.get("name") != null &&
+           obj.has("vibration") && obj.get("vibration") != null )  {
            appendDataSet(obj);
-           
+
            /*
             * if count is 1, do prediction. the co-related forecast is 1.
             * else get forecast from the previous prediction using count value
             */
            if (this.count == 1) {
-        	   
+
 	           String ret = post(this.getAccessURL(), data.toString());
-	
+
 	          /* return from rest call will be the following:
-	           * [{"header":["TEMPERATURE","field3","$TI_TimeIndex","$TI_TimeLabel","$TI_Cycle","$TI_Period","$TI_Future","$TS-TEMPERATURE","$TSLCI-TEMPERATURE","$TSUCI-TEMPERATURE"],
+	           * [{"header":["vibration","field3","$TI_TimeIndex","$TI_TimeLabel","$TI_Cycle","$TI_Period","$TI_Future","$TS-vibration","$TSLCI-vibration","$TSUCI-vibration"],
                    * [{"header":["TEMPRATURE","SEQ","$TI_TimeIndex","$TI_TimeLabel","$TI_Period","$TI_Future","$TS-TEMPRATURE","$TSLCI-TEMPRATURE","$TSUCI-TEMPRATURE"]
 	           *   "data":[
                    *      [17.69,1,1,"Period 1",1,0,17.68999888446639,17.583592583375612,17.79640518555717],
@@ -166,40 +166,40 @@ public class IoTPredictionNonPeriodic extends IoTPrediction {
 	           * }]
 	           *
 	           */
-	
+
 	           //remove the first "[" and last "]" from return string
 	           if (ret.length() > 0 && ret.charAt(0) == '[' && ret.charAt(ret.length()-1) == ']') {
 	               ret = ret.substring(1, ret.length()-1);
 	           }
-	           JSONObject prediction = new JSONObject(ret);	
+	           JSONObject prediction = new JSONObject(ret);
 	           pa = (JSONArray) prediction.get("data");
-	
+
 	           if (pa.length() < cycle) {
 	               System.out.println("Wrong prediction output with " + pa.length() + " entries, while prediction cycle is " + cycle);
 	               throw new JSONException("Wrong prediction output with " + pa.length() + " entries, while prediction cycle is " + cycle);
-	          } 
+	          }
           }
-           
+
           //get corresponded prediction from array
           if (pa.isNull(this.count-1) == true) {
         	  System.out.println("the JSONArray is empty with index " + (this.count-1));
-        	  
+
           } else {
 	          JSONArray pj = pa.getJSONArray(this.count-1);
 	          String pstr = pj.toString();
-	          System.out.println("the string is *" + pstr + "*"); 
+	          System.out.println("the string is *" + pstr + "*");
 	          String[] parts = pstr.split(",");
-	          //$TS-TEMPERATURE is the 7th in the array
+	          //$TS-vibration is the 7th in the array
 	          //However 5th will be splitted into 2
 	          if (parts.length < 7) {
 	              forecast = 0.0;
 	          } else {
 	              forecast = Double.parseDouble(parts[6]);
 	          }
-	
-	          double current = obj.getDouble("temperature");
+
+	          double current = obj.getDouble("vibration");
               Double zscore = zscoreObj.zScore(current-forecast);
-             
+
               pdt = new JSONObject(obj);
               pdt.put("forecast", forecast);
               if(zscore == null || zscore.isNaN() || zscore == 0) {
@@ -215,7 +215,7 @@ public class IoTPredictionNonPeriodic extends IoTPrediction {
         		  }
             	  pdt.put("zscore", zscore.doubleValue());
               }
-              
+
               if (windowsize > 0) {
             	  Double wzscore = zscoreObj.windowZScore(current-forecast);
             	  if(wzscore == null || wzscore.isNaN() || wzscore == 0) {
@@ -231,22 +231,22 @@ public class IoTPredictionNonPeriodic extends IoTPrediction {
             		  }
             		  pdt.put("wzscore", wzscore.doubleValue());
             	  }
-              } 
+              }
               System.out.println("[" + this.count +"] JSON to RTI:" + pdt.toString());
           }
-          
-          
+
+
           //update count
           this.count++;
-          
+
           if (this.count > cycle) {
         	  //set count back to 1
         	  this.count = this.count-cycle;
           }
           this.pdt = pdt.toString();
           return pdt.toString();
-          
-       } 
+
+       }
 
        return null;
     }
@@ -261,31 +261,31 @@ public class IoTPredictionNonPeriodic extends IoTPrediction {
     public static void main(String[] args) {
 
       final List<String> input = Arrays.asList(
-    		  "{\"name\":\"datacenter\",\"temperature\":17.47}",
-    		  "{\"name\":\"datacenter\",\"temperature\":17.47}",
-    		  "{\"name\":\"datacenter\",\"temperature\":17.44}",
-    		  "{\"name\":\"datacenter\",\"temperature\":17.44}",
-    		  "{\"name\":\"datacenter\",\"temperature\":17.38}",
-    		  "{\"name\":\"datacenter\",\"temperature\":17.44}",
-    		  "{\"name\":\"datacenter\",\"temperature\":17.47}",
-    		  "{\"name\":\"datacenter\",\"temperature\":17.5}",
-    		  "{\"name\":\"datacenter\",\"temperature\":17.5}",
-    		  "{\"name\":\"datacenter\",\"temperature\":17.5}",
-    		  "{\"name\":\"datacenter\",\"temperature\":17.5}",
-    		  "{\"name\":\"datacenter\",\"temperature\":17.5}",
-    		  "{\"name\":\"datacenter\",\"temperature\":17.56}",
-    		  "{\"name\":\"datacenter\",\"temperature\":17.56}",
-    		  "{\"name\":\"datacenter\",\"temperature\":17.53}",
-    		  "{\"name\":\"datacenter\",\"temperature\":17.5}",
-		      "{\"name\":\"datacenter\",\"temperature\":17.53}",
-    		  "{\"name\":\"datacenter\",\"temperature\":17.5}",
-    		  "{\"name\":\"datacenter\",\"temperature\":17.53}",
-    		  "{\"name\":\"datacenter\",\"temperature\":17.47}",
-    		  "{\"name\":\"datacenter\",\"temperature\":17.47}",
-    		  "{\"name\":\"datacenter\",\"temperature\":17.47}",
-    		  "{\"name\":\"datacenter\",\"temperature\":17.47}",
-    		  "{\"name\":\"datacenter\",\"temperature\":17.41}",
-    		  "{\"name\":\"datacenter\",\"temperature\":17.42}"
+    		  "{\"name\":\"datacenter\",\"vibration\":17.47}",
+    		  "{\"name\":\"datacenter\",\"vibration\":17.47}",
+    		  "{\"name\":\"datacenter\",\"vibration\":17.44}",
+    		  "{\"name\":\"datacenter\",\"vibration\":17.44}",
+    		  "{\"name\":\"datacenter\",\"vibration\":17.38}",
+    		  "{\"name\":\"datacenter\",\"vibration\":17.44}",
+    		  "{\"name\":\"datacenter\",\"vibration\":17.47}",
+    		  "{\"name\":\"datacenter\",\"vibration\":17.5}",
+    		  "{\"name\":\"datacenter\",\"vibration\":17.5}",
+    		  "{\"name\":\"datacenter\",\"vibration\":17.5}",
+    		  "{\"name\":\"datacenter\",\"vibration\":17.5}",
+    		  "{\"name\":\"datacenter\",\"vibration\":17.5}",
+    		  "{\"name\":\"datacenter\",\"vibration\":17.56}",
+    		  "{\"name\":\"datacenter\",\"vibration\":17.56}",
+    		  "{\"name\":\"datacenter\",\"vibration\":17.53}",
+    		  "{\"name\":\"datacenter\",\"vibration\":17.5}",
+		      "{\"name\":\"datacenter\",\"vibration\":17.53}",
+    		  "{\"name\":\"datacenter\",\"vibration\":17.5}",
+    		  "{\"name\":\"datacenter\",\"vibration\":17.53}",
+    		  "{\"name\":\"datacenter\",\"vibration\":17.47}",
+    		  "{\"name\":\"datacenter\",\"vibration\":17.47}",
+    		  "{\"name\":\"datacenter\",\"vibration\":17.47}",
+    		  "{\"name\":\"datacenter\",\"vibration\":17.47}",
+    		  "{\"name\":\"datacenter\",\"vibration\":17.41}",
+    		  "{\"name\":\"datacenter\",\"vibration\":17.42}"
       );
       int cycle = Integer.parseInt(args[0]);
       System.out.println("prediction cycle is " + cycle);
@@ -293,7 +293,7 @@ public class IoTPredictionNonPeriodic extends IoTPrediction {
 
       try {
     	  prediction.load(cycle);
-    	  
+
     	  for (int i = 0; i < input.size(); i++) {
               //String pdt = prediction.predict(newline);
               System.out.println("input index:" + i);
@@ -306,7 +306,6 @@ public class IoTPredictionNonPeriodic extends IoTPrediction {
           ex.printStackTrace();
       }
     }
-    */    
+    */
 
 }
-
